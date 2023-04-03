bafb391cb61d96c8b425890ef01190bd93999ca1
adancard <125130778+adancard@users.noreply.github.com>
2023-04-03 11:17:08 -0300

    Add files via upload

--- array/src/array/Tabela_nota.java
+++ array/src/array/Tabela_nota.java
@@ -3,12 +3,57 @@ package array;
 import java.util.Scanner;
 
 public class Tabela_nota {
+	
+		public static float [][]calcularMedia(float [][]arrayNotas, int quantidadeDeAlunos, int quantidadeDeNotas) {
+			
+			float [][]notasAlunos = new float [quantidadeDeAlunos][1];
+			float total=0;
+			
+			for(int i = 0; i < quantidadeDeAlunos; i++) {
+				for(int j = 0; j < quantidadeDeNotas; j++) {
+					total = total + arrayNotas[i][j];
+				}
+				notasAlunos[i][0]= (total/quantidadeDeNotas);
+			}
 
-	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
+			return notasAlunos;
+		}
+		
 
-		
-		
+		public static void main(String[] args) {
+			int quantidadeDeAlunos, quantidadeDeNotas;
+			float [][]arrayNotas;
+			float [][]arrayNotasFinal;
+			float notaTurma = 0, mediaTurma = 0;
+			
+			Scanner lerDados = new Scanner(System.in);
+			
+			System.out.println("Informe a quantidade de Alunos: ");
+			quantidadeDeAlunos = lerDados.nextInt();
+			
+			System.out.println("Informe a quantidade de notas: ");
+			quantidadeDeNotas = lerDados.nextInt();
+			
+			arrayNotas = new float[quantidadeDeAlunos][quantidadeDeNotas];
+			
+			for(int i = 0; i < quantidadeDeAlunos; i++) {
+				for(int j = 0; j < quantidadeDeNotas; j++) {
+					System.out.printf("\nInforme a quantidade de nota[%d] do aluno %d: ",j,i);
+					arrayNotas[i][j] = lerDados.nextFloat(); 
+				}
+			}
+			
+			arrayNotasFinal = calcularMedia(arrayNotas, quantidadeDeAlunos, quantidadeDeNotas);
+			
+		    System.out.println("Medias: ");
+			for(int i = 0; i < quantidadeDeAlunos; i++) {
+				System.out.println(arrayNotasFinal[i][0]);
+				notaTurma = notaTurma + arrayNotasFinal[i][0];
+			}
+			
+			mediaTurma = notaTurma/quantidadeDeAlunos;
+			System.out.println("media Turma: "+mediaTurma);
+			lerDados.close();
+		}
 	}
 
-}
--- array/src/array/Temperatura.java
+++ array/src/array/Temperatura.java
@@ -5,75 +5,67 @@ import java.util.Scanner;
 public class Temperatura {
 
 	public static void main(String[] args) {
+		
 		float[] array = new float[1];
 		Scanner entrada = new Scanner(System.in);
-		float temp1,temp2,temp3,maior=0,menor=1000;
-		
-		for(int i = 0; i < array.length;i++) {
-			
-			System.out.println("Temperatura de  Manha: ");
-			temp1=entrada.nextFloat();
-			array[i]=temp1;
-			
-			System.out.println("Temperatura de  Tarde: ");
-			temp2=entrada.nextFloat();
-			array[i]=temp2;
-			
-			System.out.println("Temperatura de  Noite: ");
-			temp3=entrada.nextFloat();
-			array[i]=temp3;
-			
-			if(temp1>temp2) {
-				
-				maior=temp1;
-				
+		float temp1, temp2, temp3, maior = 0, menor = 1000;
+
+		for (int i = 0; i < array.length; i++) {
+
+			System.out.println("Temperatura de  Manha: ");      
+			array[i] = temp1 = entrada.nextFloat();             
+                                                                
+			System.out.println("Temperatura de  Tarde: ");      
+			array[i] = temp2 = entrada.nextFloat();             
+                                                                
+			System.out.println("Temperatura de  Noite: ");      
+			array[i] = temp3 = entrada.nextFloat();             
+
+			if ((temp1 > temp2) && (temp1 > temp3)) {
+
+				maior = temp1;
+
 			}
-			
-			else if (maior>temp1){
-			
+
+			else if ((temp2 > temp1) && (temp2 > temp3)) {
+
 				maior = temp2;
-				
+
 			}
-			
-			else{
-				
+
+			else if ((temp3 > temp1) && (temp3 > temp2)) {
+
 				maior = temp3;
-				
+
 			}
 			
-			
-			if(temp1<temp2) {
-				
-				menor=temp1;
-				
+
+			if ((temp1 < temp2) && (temp1 < temp3)) {
+
+				menor = temp1;
+
 			}
-			
-			else if (temp2<temp1){
-			
+
+			else if ((temp2 < temp1) && (temp2 < temp3)) {
+
 				menor = temp2;
-				
+
 			}
-			
-			else{
-				
+
+			else {
+
 				menor = temp3;
-				
+
 			}
-			
-			
-		}
-		
-		
-		for(int i = 0; i < array.length; i++) {
-			
-			System.out.println("Maior Temperatura: "+maior);
-			System.out.println("Menor Temperatura: "+menor);
-			System.out.println(array[i]);
 
-			
 		}
 
-		
+		for (int i = 0; i < array.length; i++) {
+
+			System.out.println("Maior Temperatura: " + maior);
+
+		}
+
 		entrada.close();
 
 	}
